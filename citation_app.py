193fe2dda847987e6e92684f0417a93c12e58328
Marie Røsok <marie.rosok@nb.no>
2023-08-22 15:12:30 +0200

    Update citation_app.py
    Update text_input

--- citation_app.py
+++ citation_app.py
@@ -5,7 +5,7 @@ import citation_finder as cf
 
 st. set_page_config(layout="wide")
 
-subject=st.text_input('Angi et temaord', 'film*', help='No va du flink')
+subject=st.text_input('Søk på temaord', 'Skriv søkeord', help='Du kan bruke * som wildcard.')
 st.write(subject)
 
 korpus = dh.Corpus(doctype='digibok', limit=10, subject=subject)
