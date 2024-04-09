db8de0fc93941ffb145f78bc76eb80b92fc681e1
Marie Røsok <marie.rosok@nb.no>
2024-04-09 18:45:57 +0200

    update streamlit app with corp_limit, yearspan, conc_limit, descriptions and more

--- citation_app.py
+++ citation_app.py
@@ -1,19 +1,41 @@
 import dhlab as dh
 import streamlit as st
 import pandas as pd
+import datetime
 import citation_finder as cf
 
-st. set_page_config(layout="wide")
+st.set_page_config(layout="wide")
+
+st.title('Citation-finder')
+
+st.subheader('Lag korpus')
 
 subject=st.text_input('Søk på temaord', 'Søk', help='Skriv ønsket søkeord for tema. Du kan bruke * som wildcard før eller etter søkeordet, f.eks. "språk*"')
-st.write(subject)
 
-korpus = dh.Corpus(doctype='digibok', limit=500, subject=subject)
+corp_limit=st.number_input('Antall verk', value=500, help='Sett maksgrense for antall verk i korpuset')
 
-df = cf.citation_finder(korpus)
+st.subheader('Konkordansevalg')
+st.markdown('Velg årsspennet som skal brukes for å søke etter konkordanser og hvor mange konkordanser som skal hentes ut. Citation-finder finner siteringer i konkordansene.')
+
+curr_year=datetime.datetime.today().year
+from_year=st.number_input('Fra år', value=1000, help='Velg startår for årsspennet som skal brukes i konkordansesøket')
+to_year=st.number_input('Til år', value=curr_year, help='Velg sluttår for årsspennet som skal brukes i konkordansesøket')
+yearspan=(from_year,to_year)
+
+conc_limit=st.number_input('Antall konkordanser', value=4000, help='Sett maksgrense for antall konkordanser som skal hentes ut')
+
+
+st.write(subject, corp_limit, from_year, to_year, conc_limit)
+
+
+
+corpus = dh.Corpus(doctype='digibok', subject=subject, limit=corp_limit)
+
+
+df = cf.citation_finder(corpus, yearspan=yearspan, conc_limit=conc_limit)
 df.columns = ["urn","citation"]
-res = df.merge(korpus.frame, left_on='urn', right_on='urn')['urn title authors year citation'.split()]
+
+res = df.merge(corpus.frame, left_on='urn', right_on='urn')['urn title author year citation'.split()]
 
 
 st.dataframe(res)
-
